c1b987f7c382a3c7796785a5ef3338354daa71a6
Guillem MC <guimarca@gmail.com>
2021-12-30 22:33:13 +0100

    Initial commit

---
+++ python_playground/web.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+# FLASK_APP=python_playground.web flask run --host=0.0.0.0 --port=8001
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
