d24e08d572d42a8911c15abd3fb9fc1631bc7fd0
Guillem Martinez <guillem@antswarm.ai>
2023-11-22 09:47:12 +0100

    Add ruff config

--- python_playground/playground.py
+++ python_playground/playground.py
@@ -1,1 +1,12 @@
+import typing as t
+
+
+def my_func() -> t.Any:
+    return
+
+
 print("Python test playground")
+
+print(
+    "this is a very long line this is a very long line this is a very long line this is a very long line this is a very"
+)
